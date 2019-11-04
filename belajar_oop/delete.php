8bd1da9d47e6593d9e3c6b6e4726a1700d8d37ba
Robby Legitra Kurniawan <48901437+robbylegitra@users.noreply.github.com>
2019-11-04 19:32:33 +0700

    Update delete.php

--- belajar_oop/delete.php
+++ belajar_oop/delete.php
@@ -4,7 +4,4 @@ $id = $_GET["id"];
 if ($siswa->hapus($id)) {
     echo "<script>alert('Delete Data');</script>";
     echo "<script>location='tampilsiswa.php';</script>";
-} else {
-    echo "<script>alert('Failed');</script>";
-    echo "<script>location='tampilsiswa.php';</script>";
 }
